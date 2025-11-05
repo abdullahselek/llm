6a3127d778ffcbc2821634e50066c5a0b9b1bcd1
abdullahselek <abdullahselek@gmail.com>
2025-11-05 18:04:50 +0000

    New fixture, vocablary raw text.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 """PyTest Configurations."""
 
+from pathlib import Path
+
 import pytest
 
 from llm.utils import create_vocab
@@ -9,3 +11,12 @@ from llm.utils import create_vocab
 def vocab() -> dict:
     """Vocablary dict."""
     return create_vocab()
+
+
+@pytest.fixture()
+def vocab_text() -> str:
+    """Vocablary raw text."""
+    file_path = Path.cwd() / "src/llm/resources/the-verdict.txt"
+    with open(str(file_path), encoding="utf-8") as f:
+        text = f.read()
+    return text
