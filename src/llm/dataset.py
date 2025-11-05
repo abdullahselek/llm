b18a3efa09cda82904346cdf8ef51cf291c3bf8f
abdullahselek <abdullahselek@gmail.com>
2025-11-05 18:54:52 +0000

    Function that creates GPT dataloader.

--- src/llm/dataset.py
+++ src/llm/dataset.py
@@ -1,7 +1,7 @@
-"""GPT Dataset."""
+"""GPT Dataset and DataLoader."""
 
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, Dataset
 
 from llm.bpe_tokenizer import BPETokenizer
 
@@ -40,3 +40,38 @@ class GPTDataset(Dataset):
     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
         """Input ids and target ids at index."""
         return self.input_ids[idx], self.target_ids[idx]
+
+
+def create_gpt_dataloader(
+    text: str,
+    batch_size: int = 4,
+    max_length: int = 256,
+    stride: int = 128,
+    shuffle: bool = True,
+    drop_last: bool = True,
+    num_workers: int = 4,
+) -> DataLoader:
+    """Create a DataLoader for GPTDataset.
+
+    Args:
+        text (str): Raw vocablary text.
+        batch_size (int): Batch size, default 4.
+        max_length (int): Chunk length, default 256.
+        stride (int): Length of overflowing token, default 128.
+        shuffle (bool): shuffle data in every epoch, default True.
+        drop_last (bool): Drops last incomplete batch, default True.
+        num_workers (int): Number of subprocesses to load the data, default 4.
+
+    Returns:
+        DataLoader object.
+
+    """
+    tokenizer = BPETokenizer()
+    dataset = GPTDataset(text=text, tokenizer=tokenizer, max_length=max_length, stride=stride)
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers,
+    )
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,7 +1,7 @@
 """Tests for dataset module."""
 
 from llm.bpe_tokenizer import BPETokenizer
-from llm.dataset import GPTDataset
+from llm.dataset import GPTDataset, create_gpt_dataloader
 
 
 def test_GPTDataset_loading_items(vocab_text: str):
@@ -19,3 +19,17 @@ def test_GPTDataset_getitem_at_index(vocab_text: str):
     item = dataset[0]
 
     assert len(item) == 2
+
+
+def test_create_gpt_dataloader(vocab_text: str):
+    """Test creating GPT data loader with raw text."""
+    data_loader = create_gpt_dataloader(vocab_text, batch_size=4, max_length=256, stride=128)
+    data_iter = iter(data_loader)
+    first_batch = next(data_iter)
+
+    assert len(first_batch) == 2
+    assert len(first_batch[0]) == 4
+    assert len(first_batch[-1]) == 4
+
+    assert len(first_batch[0][0]) == 256
+    assert len(first_batch[0][-1]) == 256
