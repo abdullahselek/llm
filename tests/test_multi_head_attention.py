d3a00f10039b30c8e55467d39546bf91c405b478
abdullahselek <abdullahselek@gmail.com>
2025-11-08 19:40:09 +0000

    Add a new test case for MultiHeadAttention.

--- tests/test_multi_head_attention.py
+++ tests/test_multi_head_attention.py
@@ -5,6 +5,19 @@ import torch
 from llm.multi_head_attention import MultiHeadAttention
 
 
+def test_MultiHeadAttention_initilization():
+    """Test initialization of MultiHeadAttention."""
+    multi_head_attention = MultiHeadAttention(
+        input_dim=3,
+        output_dim=2,
+        context_length=8,
+        dropout=0.1,
+        num_heads=4,
+    )
+
+    assert len(multi_head_attention.heads) == 4
+
+
 def test_MultiHeadAttention_compute_context_vectors(
     token_ids: list[int], batch_embeddings: torch.Tensor
 ):
@@ -25,4 +38,7 @@ def test_MultiHeadAttention_compute_context_vectors(
 
     context_vectors = multi_head_attention(batch_embeddings)
 
+    # token_ids are used in generating token_embeddings and
+    # batch_embeddings are combination of two token_embeddings
+    # they are available in conftest.py
     assert context_vectors.shape == (2, len(token_ids), output_dim * len(batch_embeddings))
