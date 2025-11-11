7b1facb630c42037ecfdb407988ce07e743a4ae5
abdullahselek <abdullahselek@gmail.com>
2025-11-11 18:34:42 +0000

    Support use of KV cache.

--- src/llm/transformer_block.py
+++ src/llm/transformer_block.py
@@ -31,6 +31,7 @@ class TransformerBlock(nn.Module):
         n_heads: int,
         drop_rate: float,
         qkv_bias: bool,
+        window_size: int | None = None,
     ):
         """Initialze TransformerBlock object.
 
@@ -40,6 +41,8 @@ class TransformerBlock(nn.Module):
             n_heads (int): Number of attention heads in multi-head attention.
             drop_rate (float): Dropout probability for regularization.
             qkv_bias (bool): Whether to include bias terms in QKV linear projections.
+            window_size (int | None): Size of the sliding window for KV cache.
+                If None, uses max_seq_len.
 
         """
         super().__init__()
@@ -50,18 +53,20 @@ class TransformerBlock(nn.Module):
             dropout=drop_rate,
             num_heads=n_heads,
             qkv_bias=qkv_bias,
+            window_size=window_size if window_size else context_length,
         )
         self.feed_forward = FeedForward(embedding_dim=embedding_dim)
         self.layer_norm1 = LayerNorm(embedding_dim=embedding_dim)
         self.layer_norm2 = LayerNorm(embedding_dim=embedding_dim)
         self.dropout = nn.Dropout(drop_rate)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, use_cache: bool = False) -> torch.Tensor:
         """Apply the transformer block transformation to the input tensor.
 
         Args:
             x (torch.Tensor): Input tensor of shape
                 (batch_size, sequence_length, embedding_dim).
+            use_cache (bool): Enable using KV cache. Defaults to False.
 
         Returns:
             torch.Tensor: Output tensor of the same shape as input.
@@ -70,7 +75,7 @@ class TransformerBlock(nn.Module):
         # First residual connection with attention
         shortcut = x
         x = self.layer_norm1(x)
-        x = self.multi_head_attention(x)
+        x = self.multi_head_attention(x, use_cache=use_cache)
         x = self.dropout(x)
         x = x + shortcut
 
