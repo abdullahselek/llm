00dffaa6c68e5ec0597f6fe3efc4128cf38bcb8f
abdullahselek <abdullahselek@gmail.com>
2025-11-19 20:23:45 +0000

    Load Python datasets from HF.

--- src/llm/train.py
+++ src/llm/train.py
@@ -1,1 +1,16 @@
 """Model training module."""
+
+import os
+
+from datasets import load_dataset
+from dotenv import load_dotenv
+
+load_dotenv()
+
+dataset = load_dataset(
+    "bigcode/starcoderdata",
+    data_dir="python",
+    split="train",
+    token=os.getenv("HF_TOKEN"),
+)
+print(f"dataset: {dataset}")
